52f4673134639d42a29949a93e9c6df9550b4a6c
Kuan <77795861+kuan-Wu@users.noreply.github.com>
2022-02-26 20:34:25 -0500

    Add files via upload

---
+++ inclass-4/site/includes/footer.php
@@ -0,0 +1,69 @@
+<footer>
+    <div class="section-container footer-container">
+        <div class="container">
+            <div class="row">
+                    <div class="col-md-4">
+                        <h4>About us</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet consectetur dolor</p>
+                    </div>
+
+                    <div class="col-md-4">
+                        <h4>Do you like ? Share this !</h4>
+                        <p>
+                            <a href="https://facebook.com/" class="social-round-icon white-round-icon fa-icon" title="">
+                            <i class="fa fa-facebook" aria-hidden="true"></i>
+                          </a>
+                          <a href="https://twitter.com/" class="social-round-icon white-round-icon fa-icon" title="">
+                            <i class="fa fa-twitter" aria-hidden="true"></i>
+                          </a>
+                          <a href="https://www.linkedin.com/" class="social-round-icon white-round-icon fa-icon" title="">
+                            <i class="fa fa-linkedin" aria-hidden="true"></i>
+                          </a>
+                        </p>
+                        <p><small>© Untitled <?php echo date('Y'); ?>| <a href="http://www.unsplash.com/" class="link-like-text" title="Beautiful Free Images">Unsplash</a></small></p>    
+                    </div>
+
+                    <div class="col-md-4">
+                        <h4>Subscribe to newsletter</h4>
+                        
+                        <div class="form-group">
+                            <div class="input-group">
+                                <input type="text" class="form-control footer-input-text">
+                                <div class="input-group-btn">
+                                    <button type="button" class="btn btn-primary btn-newsletter ">OK</button>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+</body>
+<!-- body tag end -->
+</html>
+
+<?php
+// set the default timezone to use.
+date_default_timezone_set('UTC');
+
+
+// Prints something like: Monday
+echo date("l") . "<br>";
+
+// Prints something like: Monday 8th of August 2005 03:12:46 PM
+echo date('l jS \of F Y h:i:s A') . "<br>";
+
+// Prints: July 1, 2000 is on a Saturday
+echo "July 1, 2000 is on a " . date("l", mktime(0, 0, 0, 7, 1, 2000)) . "<br>";
+
+/* use the constants in the format parameter */
+// prints something like: Wed, 25 Sep 2013 15:28:57 -0700
+echo date(DATE_RFC2822)  . "<br>";
+
+// prints something like: 2000-07-01T00:00:00+00:00
+echo date(DATE_ATOM, mktime(0, 0, 0, 7, 1, 2000)) . "<br>";
+?>
